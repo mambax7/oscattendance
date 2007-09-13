6b5064867de4c729d454b56561499f232f9d9aae
srmcatee <srmcatee@9b3c414f-5b12-0410-97f9-d54092e68bf8>
2007-09-13 16:01:57 +0000

    Fixed Block phrase error lookup.  New problem in latest xoops release.

--- language/english/blocks.php
+++ language/english/blocks.php
@@ -5,4 +5,7 @@ define("_oscatt_nav_attendance","Attendance");
 define("_oscatt_nav_reports","Reports");
 define("_oscatt_nav_block_title","OSC Attendance");
 define("_oscatt_nav_barcodeattendance","Barcode Attendance");
+define("_oscatt_nav_recordattendance","Record Attendance");
+define("_oscatt_nav_manageattendance","Manage Attendance");
+
 ?>
