8536b8612dfc4450fb07b7270ffdc0e15118864e
srmcatee <srmcatee@9b3c414f-5b12-0410-97f9-d54092e68bf8>
2009-01-04 15:23:03 +0000

    Added Polish.

---
+++ language/polish/admin.php
@@ -0,0 +1,19 @@
+<?php
+define ( "_oscatt_ADMINTITLE", "Uczestnictwo OSC - Administrator");
+//
+
+//
+define ( "_oscatt_DB_CREATED", "Tabela utworzona!");
+
+define ( "_oscatt_admin_access_denied", "Administrator Odmowa dostępu");
+
+define ( "_oscatt_permissions_view", "Wyświetl Pozwolenia");
+define ( "_oscatt_permissions_view_desc", "Pozwolenie przeglądania darowizn");
+
+define ( "_oscatt_permissions_modify", "Modyfikuj / edytuj Pozwolenie");
+define ( "_oscatt_permissions_modify_desc", "Pozwolenie na modyfikowanie i tworzenie darowizn");
+
+define ( "_oscatt_permissions_admin", "Pozwolenia Administracyjne");
+define ( "_oscatt_permissions_admin_desc", "Pozwolenia Administracyjne dla darowizn");
+
+?>
---
+++ language/polish/blocks.php
@@ -0,0 +1,11 @@
+<?php
+// Bloki
+define ( "_oscatt_nav_events", "Wydarzenia");
+define ( "_oscatt_nav_attendance", "Obecność");
+define ( "_oscatt_nav_reports", "Raporty");
+define ( "_oscatt_nav_block_title", "Obecność OSC");
+define ( "_oscatt_nav_barcodeattendance", "Kod kreskowy Obecności");
+define ( "_oscatt_nav_recordattendance", "Zapis Obecności");
+define ( "_oscatt_nav_manageattendance", "Zarządzaj Obecnością");
+
+?>
